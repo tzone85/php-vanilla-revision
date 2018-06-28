49d9b98f9219b6307f80666cb7c4f6ec963d74d6
Thando Mini <thandomini@Thandos-MacBook-Air.local>
2018-06-28 19:49:21 +0200

    separated the html into a view file and called that file using require key-word in the index.php

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+<?php
 
-    <style>
-        header {
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
+$greeting = 'Hello, world';
 
-<h1>
-
-<h1>
-    <?= "Hello, ".htmlspecialchars($_GET['name']); ?>
-</h1>
-</h1>
-
-</h1>
-</body>
-</html>
+require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <style>
+        header {
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+<h1>
+
+<h1>
+
+    <header>
+        <?= $greeting; ?>
+    </header>
+</h1>
+</h1>
+
+</h1>
+</body>
+</html>
