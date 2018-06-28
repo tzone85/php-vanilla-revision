db78bcd78ef604a4e553e928b3234a74b9eae1ea
Thando Mini <thandomini@Thandos-MacBook-Air.local>
2018-06-28 19:35:51 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <style>
+        header {
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+<h1>
+
+<h1>
+    <?= "Hello, ".$_GET['name']; ?>
+</h1>
+</h1>
+
+</h1>
+</body>
+</html>
